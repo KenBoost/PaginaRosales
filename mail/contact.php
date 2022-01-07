2497f1cf5ed563313e7b308b6725d793d0392608
Keneth Miranda <kennethmiranda56@gmail.com>
2022-01-07 14:15:08 -0600

    cambion en correo y video

--- mail/contact.php
+++ mail/contact.php
@@ -9,9 +9,9 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
+$to = "kennethmiranda56@gmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$body = "Ha recibido un mail desde su pagina web.\n\n"."Aqui estan los detalles:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";	
 
