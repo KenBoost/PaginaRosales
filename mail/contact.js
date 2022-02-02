9893e14e31f96f0eaf4ea903277da74ae9b4cf70
Keneth Miranda <kennethmiranda56@gmail.com>
2022-02-02 11:50:37 -0600

    prueba 1

--- mail/contact.js
+++ mail/contact.js
@@ -1,4 +1,44 @@
-$(function () {
+function validacion() {
+  let nombre = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let tema = document.getElementById("subject").value;
+  let mensaje = document.getElementById("message").value;
+  
+
+  debugger;
+  if (mensaje == undefined) {
+    mensaje = "Sin mensaje";
+  }
+
+  if (!nombre || !tema || !email) {
+    alert("Aún faltan datos para completar el mensaje, por favor revisar!");
+  } else {
+    const mensa =
+      "Nombre: " +
+      nombre +
+      "<br/> Tema: " +
+      tema+
+      "<br/> mensaje: " +
+      mensaje;
+
+    envioCorreo(email, mensa);
+  }
+}
+
+function envioCorreo(email, mensaje) {
+  Email.send({
+    SecureToken: "udwicytewgic3mokdmfcj",
+    To:"asadaapp2022@gmail.com",
+    From: "asadaapp2022@gmail.com",
+    Subject: "Formulario de solicitud de pagina Rosales",
+    Body: mensaje,
+  }).then((message) => alert(message));
+}
+
+
+
+
+/*$(function () {
   $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
     preventSubmit: true,
     submitError: function ($form, event, errors) {},
@@ -72,4 +112,4 @@ $(function () {
 
 $("#name").focus(function () {
   $("#success").html("");
-});
+});*/
